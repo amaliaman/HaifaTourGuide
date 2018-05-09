547b91dbbf34ce5623bb1f8518d8d3105f956b53
Ami Gilad Man <31509942+amaliaman@users.noreply.github.com>
2018-05-09 22:26:37 +0300

    Add all museums

--- app/src/main/java/com/amaliapps/haifatourguide/Initializer.java
+++ app/src/main/java/com/amaliapps/haifatourguide/Initializer.java
@@ -39,6 +39,31 @@ public class Initializer {
                 context.getString(R.string.vivino_source), context.getString(R.string.vivino_phone),
                 context.getString(R.string.vivino_url));
 
+        Location loc5 = new Location(MUSEUMS_CATEGORY_ID, context.getString(R.string.art_title), R.drawable.art,
+                context.getString(R.string.art_description), context.getString(R.string.art_address),
+                context.getString(R.string.art_source), context.getString(R.string.art_phone),
+                context.getString(R.string.art_url));
+
+        Location loc6 = new Location(MUSEUMS_CATEGORY_ID, context.getString(R.string.maritime_title), R.drawable.maritime,
+                context.getString(R.string.maritime_description), context.getString(R.string.maritime_address),
+                context.getString(R.string.maritime_source), context.getString(R.string.maritime_phone),
+                context.getString(R.string.maritime_url));
+
+        Location loc7 = new Location(MUSEUMS_CATEGORY_ID, context.getString(R.string.city_title), R.drawable.city,
+                context.getString(R.string.city_description), context.getString(R.string.city_address),
+                context.getString(R.string.city_source), context.getString(R.string.city_phone),
+                context.getString(R.string.city_url));
+
+        Location loc8 = new Location(MUSEUMS_CATEGORY_ID, context.getString(R.string.mane_title), R.drawable.mane,
+                context.getString(R.string.mane_description), context.getString(R.string.mane_address),
+                context.getString(R.string.mane_source), context.getString(R.string.mane_phone),
+                context.getString(R.string.mane_url));
+
+        Location loc9 = new Location(MUSEUMS_CATEGORY_ID, context.getString(R.string.struck_title), R.drawable.struck,
+                context.getString(R.string.struck_description), context.getString(R.string.struck_address),
+                context.getString(R.string.struck_source), context.getString(R.string.struck_phone),
+                context.getString(R.string.struck_url));
+
         isInitialized = true;
     }
 }
